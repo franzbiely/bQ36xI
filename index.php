426b13213f356164d7e53a5e0f17147e68540627
Francis Albores <mrthemetribe@gmail.com>
2017-06-05 03:21:27 +0800

    Lifespan for session after 8 hours only

--- index.php
+++ index.php
@@ -7,10 +7,19 @@
 error_reporting(0);
 error_reporting(E_ALL ^ E_WARNING); 
 
-
-
 session_start();
+
+$now = time();
+if (isset($_SESSION['discard_after']) && $now > $_SESSION['discard_after']) {
+    session_unset();
+    session_destroy();
+    session_start();
+}
+$_SESSION['discard_after'] = $now + 28800; // destroy session after 8 hours
+
+
 set_time_limit(99999);
+
 
 include("config.php");
 

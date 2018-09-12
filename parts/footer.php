7cb99d2a48a2ebec614af9ebba351bbb4ca05ec8
Francis Albores <mrthemetribe@gmail.com>
2018-09-12 22:25:59 +0800

    Update changelog

--- parts/footer.php
+++ parts/footer.php
@@ -2,7 +2,7 @@
 		<div class="row">	
 			<footer>
 				<ul>
-					<li><a href="#" style="pointer-events: none;">Susu Mamas &copy 2017</a> <span id="system_version">v3.24</span></li>			
+					<li><a href="#" style="pointer-events: none;">Susu Mamas &copy 2017</a> <span id="system_version">v3.25</span></li>			
 					<li><a href="#">Documentation</a></li>
 					<li><a href="/changelog.txt">Changelog</a></li>
 					<li><a href="#">Official Website</a></li>
